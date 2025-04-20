f444e6c36c0f36903f1f87fa3a2457b9dedd352e
Amandha Ribeiro <amandhafribeiro@gmail.com>
2025-04-20 18:56:38 -0300

    feat(p1): exibe lista ficticia

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Busca from './components/busca'
+const App = () => {
+  return (
+    <div>
+        <h1>
+        Hello P1
+            <Busca/>
+        </h1>
+    </div>
+  )
+}
+
+export default App
