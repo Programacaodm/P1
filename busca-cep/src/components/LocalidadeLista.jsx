4ccbab8b8243a1ab9efb49c764a69120bbee592e
AmandhaRibeiro <130255264+AmandhaRibeiro@users.noreply.github.com>
2025-04-25 18:52:15 -0300

    feat(p1): exibe grafico.jsx

--- busca-cep/src/components/LocalidadeLista.jsx
+++ busca-cep/src/components/LocalidadeLista.jsx
@@ -1,45 +1,66 @@
 export default function LocalidadeLista({ localidades }) {
-    return (
-      <div className="mt-4">
+    return (  
+<div className="mt-4">
         <h2>Localidades encontradas</h2>
-  
-        {localidades.length === 0 ? (
-          <p>Nenhuma localidade buscada ainda.</p>
-        ) : (
-          <ul>
-            {localidades.map((loc, index) => (
-              <li key={index}>
-                {loc.cep} <br />
-                {loc.logradouro} <br />
-                {loc.bairro} <br />
-                {loc.localidade}/{loc.uf}
-                <hr
-                  style={{
-                    width: '200px',
-                    marginLeft: '0',
-                    borderTop: '1px solid #ccc',
-                  }}
-                />
-              </li>
-            ))}
-          </ul>
-        )}
-  
-        {/* 
-        const localidadesFicticias = [
-          { cep: '01001-000', localidade: 'São Paulo' },
-          { cep: '20040-000', localidade: 'Rio de Janeiro' },
-          { cep: '30110-000', localidade: 'Belo Horizonte' }
-        ];
-  
-        <h3>Localidades Fictícias</h3>
-        <ul>
-          {localidadesFicticias.map((l, i) => (
-            <li key={i}>{l.cep} - {l.localidade}</li>
-          ))}
-        </ul>
-        */}
-      </div>
-    );
-  }
+    
+{localidades.length === 0 ? (
+    <p>Nenhuma localidade buscada ainda.</p>
+  ) : (
+    <ul>
+      {localidades.map((loc, index) => (
+        <li key={index}>
+          {loc.cep} <br />
+          {loc.logradouro} <br />
+          {loc.bairro} <br />
+          {loc.localidade}/{loc.uf}
+          <hr
+            style={{
+              width: '200px',
+              marginLeft: '0',
+              borderTop: '1px solid #ccc',
+            }}
+          />
+        </li>
+      ))}
+    </ul>
+  )}
+
+  {/*
+  const localidadesFicticias = [
+    {
+      cep: '04094-050',
+      logradouro: 'Avenida Pedro Álvares Cabral',
+      bairro: 'Parque Ibirapuera',
+      localidade: 'São Paulo',
+      uf: 'SP'
+    },
+    {
+      cep: '55592-970',
+      logradouro: 'Rua dos Navegantes',
+      bairro: 'Vila de Porto de Galinhas',
+      localidade: 'Ipojuca',
+      uf: 'PE'
+    }
+  ];
+
+  <h3>Localidades Fictícias</h3>
+  <ul>
+    {localidadesFicticias.map((l, i) => (
+      <li key={i}>
+        {l.cep} <br />
+        {l.logradouro} <br />
+        {l.bairro} <br />
+        {l.localidade}/{l.uf}
+        <hr
+          style={{
+            width: '200px',
+            marginLeft: '0',
+            borderTop: '1px solid #ccc',
+          }}
+        />
+      </li>
+    ))}
+  </ul>
+  */}
+</div>
   
